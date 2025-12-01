fbb609635c563b66507f4bc668fbfdf6fc8ede28
akasheyy <vadakkangaraakash@gmail.com>
2025-12-01 13:36:19 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 // src/lib/api.ts
 // replace: const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 
 
 interface LoginData {
